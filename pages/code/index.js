2cd8f909d7c907c4803e88d8e1e778f5b2bf4e93
akshitvilager <kumakshit15@gmail.com>
2022-05-24 00:13:46 +0530

    24may

--- pages/code/index.js
+++ pages/code/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import Code from "../../components/Code"
+import React from "react";
+import Code from "../../components/Code";
+import { useEffect } from "react";
+import Prism from "prismjs";
 
-const index = () => {
+export default function Page(props) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
+  var code1 = `var x, y, z;  // Declare 3 variables
+  x = 5;    // Assign the value 5 to x
+  y = 6;    // Assign the value 6 to y
+  z = x + y;  // Assign the sum of x and y to z
+  
+  document.getElementById("demo").innerHTML =
+  "The value of z is " + z + ".";`;
+
+  var code2 = `print("Hello world")
+print("Hello world")`;
+
   return (
-	<>
-	<Code/>
-	</>
-
-  )
+    <div className="flex justify-center">
+      <main className="max-w-2xl lg:w-[800px] sm:w-[800px] w-72">
+        <pre className="line-numbers">
+          <code className="language-jsx">{code1}</code>
+        </pre>
+        <pre className="line-numbers">
+          <code className="language-jsx">{code2}</code>
+        </pre>
+      </main>
+    </div>
+  );
 }
-
-export default index;
---
+++ pages/try.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const trys = () => {
+  return (
+    <>
+      <section className="bg-[#1E88E5] w-auto h-[750px] -skew-y-6 -my-20 "></section>
+      <section className="bg-[#00ACF3] w-full h-[750px] -skew-y-6"></section>
+      <section className="bg-[#93F188] w-full h-[750px] skew-y-6"></section>
+      <section className="bg-[#F9F871] w-full h-[750px] -skew-y-3"></section>
+
+    </>
+  );
+};
+
+export default trys;
