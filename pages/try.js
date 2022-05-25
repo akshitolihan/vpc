55f06aa273c2215dc330a5d0d1205074f826a5e5
akshitvilager <kumakshit15@gmail.com>
2022-05-25 19:05:43 +0530

    bn

--- pages/try.js
+++ pages/try.js
@@ -3,10 +3,10 @@ import React from "react";
 const trys = () => {
   return (
     <>
-      <section className="bg-[#1E88E5] w-auto h-[750px] -skew-y-6 -my-20 "></section>
-      <section className="bg-[#00ACF3] w-full h-[750px] -skew-y-6"></section>
-      <section className="bg-[#93F188] w-full h-[750px] skew-y-6"></section>
-      <section className="bg-[#F9F871] w-full h-[750px] -skew-y-3"></section>
+      <section className="bg-[#ffffff] w-auto h-[750px] -skew-y-6 -my-20 "></section>
+      <section className="bg-[#000000] w-full h-[750px] -skew-y-6"></section>
+      <section className="bg-[#ffffff] w-full h-[750px] skew-y-6"></section>
+      <section className="bg-[#000000] w-full h-[750px] -skew-y-3"></section>
 
     </>
   );
