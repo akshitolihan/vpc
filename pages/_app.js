83953a8731b37cc4883b28e5cfad77a00916c8e5
akshitvilager <kumakshit15@gmail.com>
2022-06-06 15:18:18 +0530

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
-// import '../styles/globals.css'
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
 import "../styles/globals.css";
-// import "../styles/tomorrow.css";
 import "prismjs/themes/prism-tomorrow.min.css";
-
 import { useEffect } from "react";
 import Script from "next/script";
 import { useRouter } from "next/router";
@@ -41,18 +31,24 @@ function MyApp({ Component, pageProps }) {
             gtag('config', 'G-DFZ3EC0TFK');
         `}
       </Script>
-
-      <script
-        async
+{/* 
+      <Script
+        async="true"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6898963328156558"
-      ></script>
+      ></Script> */}
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-6898963328156558"
+        async="true"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <NextNProgress
         color="#1DE9B6"
         startPosition={0.5}
         stopDelayMs={100}
         height={3}
         showOnShallow={true}
-
       />
 
       <Component {...pageProps} />
