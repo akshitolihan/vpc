ab541af212dc431c0ff46f75173cd03e226391fc
akshitvilager <kumakshit15@gmail.com>
2022-06-11 20:12:04 +0530

    end

--- components/End.js
+++ components/End.js
@@ -9,7 +9,7 @@ const End = () => {
         // style={{ fontFamily: "Georgia serif" }}
         className="text-justify text-[26px] font-semibold text-[#1DE9B6] p-0 py-4 "
       >
-        <section className="rounded-xl shadow-lg h-[6px] w- max-w-[6000px] bg-[#1DE9B6] mb-2"></section>
+        <section className="rounded-xl shadow-lg h-[6px] w-auto max-w-[600px] bg-[#1DE9B6] mb-2"></section>
         <section>
           Thank You <br />
           Akshit Kumar rests his words here...
