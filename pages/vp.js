5056576c0c37318f619fbb37eb404a6ab0159efb
akshitvilager <kumakshit15@gmail.com>
2022-07-19 15:14:01 +0530

    as

---
+++ pages/vp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar from "../styles/Navbar.module.css";
+
+const vp = () => {
+  return (
+    <>
+        <h1 id={navbar.edu} className="text-[100px] font-bold">VillagePur</h1>
+    </>
+  )
+}
+
+export default vp;
