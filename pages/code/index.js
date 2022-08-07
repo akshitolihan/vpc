e663ec564d477e8604877b9f93fe1de5351ec833
akshitvilager <kumakshit15@gmail.com>
2022-08-07 15:39:50 +0530

    gf

--- pages/code/index.js
+++ pages/code/index.js
@@ -3,7 +3,6 @@ import Head from "next/dist/shared/lib/head";
 import Navbar from "../../components/Navbar";
 import Code from "../../components/Code";
 import { useEffect, useState } from "react";
-import Prism from "Prismjs";
 import Heading from "../../components/Heading";
 import Heading2 from "../../components/Heading2";
 import Image from "next/dist/client/image";
