3fda727472667709d8f2953199242ea2e4381772
akshitvilager <kumakshit15@gmail.com>
2022-08-10 23:08:20 +0530

    uh

--- components/Code.js
+++ components/Code.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import Prism from "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/themes/prism-tomorrow.min.css";
 import "prismjs/components/prism-jsx.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
